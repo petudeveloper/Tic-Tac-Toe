b9f49eaa053e781864e6b60c0ef66209498bb74d
Fondem Junior <fondemjunior@yahoo.com>
2021-05-11 22:46:44 +0100

    Add correct judge_spec

--- spec/judge_spec.rb
+++ spec/judge_spec.rb
@@ -16,9 +16,27 @@ describe Judge do
         end
     end
     describe '#valid_move(move, array)' do
-            it 'returns a false when the number entered is in the array and already attributed' do
+            it 'returns a false when the number entered is in the array of moves' do
             judge = Judge.new
             expect(judge.valid_move(1, 1..9)).to eql(false)
             end
+            it 'returns moves when the number is not already attributed' do
+            judge = Judge.new
+            expect(judge.valid_move(1, 2..9)).to eql(1)
+            end
+            it 'returns false when the number is not in the range of valid moves' do
+            judge = Judge.new
+            expect(judge.valid_move(10, 2..9)).to eql(false)
+            end
+    end
+    describe '#check_winner'do
+        it 'returns a true when the array of moves is a winner_scenario' do
+            judge = Judge.new
+            expect(judge.check_winner([3, 5, 6, 7])).to eql(true)
+        end
+        it 'returns a false when the array of moves is not a winner_scenario' do
+            judge = Judge.new
+            expect(judge.check_winner([4, 5, 7])).to eql(false)
+        end
     end
 end
